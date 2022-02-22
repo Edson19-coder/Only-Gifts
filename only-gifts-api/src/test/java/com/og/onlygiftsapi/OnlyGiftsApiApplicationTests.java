240d48089cad05ac820754410bf9a53d7892a992
Edson Juan Lugo Sánchez <edson.lugo@netpay.com.mx>
2022-02-22 00:19:35 -0600

    Init Only Gifts

---
+++ only-gifts-api/src/test/java/com/og/onlygiftsapi/OnlyGiftsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.og.onlygiftsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyGiftsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
