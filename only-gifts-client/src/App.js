6192cb5c05c7d0cb9c7ce31ccc8c1db9870c2671
Edson Lugo <edson.lugo@netpay.com.mx>
2022-05-25 01:07:38 -0500

    commit

--- only-gifts-client/src/App.js
+++ only-gifts-client/src/App.js
@@ -23,12 +23,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="product/:productId" element={<ProductItem />} />
-          <Route path="payment-method" element={<PaymentMethod />} />
-          <Route path="address" element={<Address />} />
-          <Route path="orders-history" element={<History />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/product/:productId" element={<ProductItem />} />
+          <Route path="/payment-method" element={<PaymentMethod />} />
+          <Route path="/address" element={<Address />} />
+          <Route path="/orders-history" element={<History />} />
           <Route path="/cart" element={<ShoppingCart></ShoppingCart>} />
           {/* Manager */}
           <Route path="manager/*" element={<Manager />} />
